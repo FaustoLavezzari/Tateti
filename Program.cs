93bc779612de7980048f267b7e76e0dd4ff31cee
Fausto <faustolavezzari99@hotmail.com>
2020-07-02 01:00:19 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tateti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
